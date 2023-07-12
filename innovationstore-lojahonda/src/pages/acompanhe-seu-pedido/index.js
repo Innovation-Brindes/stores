cbbdfad6960a6a7428f7224f3f92623d20c4d0eb
Alexandre Medina <alesurf13@gmail.com>
2023-07-12 09:32:13 -0300

    ajuste

--- innovationstore-lojahonda/src/pages/acompanhe-seu-pedido/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import AcompanheSeuPedido from "../../components/AcompanheSeuPedido";
-
-export default function AcompanheSeuPedidoPage(props) {
-  return <AcompanheSeuPedido hash_pedido={null} />;
-}
